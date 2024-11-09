22503495598570478728c91b88f80c5647aae867
MrVannu <ilvannu03@gmail.com>
2024-11-09 11:19:49 +0100

    v7

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template
+import pandas as pd
+
+
+output_df = pd.read_csv('resources/trasporti_ottimizzati.csv')
 
 
 
@@ -7,8 +11,10 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def welcome():
-    return render_template('map.jinja2') 
+def display_output():
+    # Convert DataFrame to HTML table format for easy rendering
+    table_html = output_df.to_html(classes='table table-striped', index=False)
+    return render_template('map.jinja2', table_html=table_html)
 
 
 if __name__ == "__main__":
